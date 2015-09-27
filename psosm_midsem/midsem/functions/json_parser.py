deea0aab270aa89a4a08069523f1cf2e958995ca
vedantswain <vedant12118@iiitd.ac.in>
2015-09-27 11:32:58 +0530

    new functions folder

---
+++ psosm_midsem/midsem/functions/json_parser.py
@@ -0,0 +1,12 @@
+import json
+
+def fileParser(file_name):
+	data = []
+	with open(file_name) as f:
+	    for line in f:
+	        data.append(json.loads(line))
+	return data
+
+if __name__=='__main__':
+	# ### returns dictionary for given json file
+	print fileParser("Bhubaneswar school_pagesearch_data.json")
